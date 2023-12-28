70efce51dca6aef41806ceed2a3c5caee6ba25da
eperez <eperez@abstractsolutions.com.ar>
2023-12-27 21:40:41 -0300

    feat: colores  y movimentos laterales

--- frontend/pages/challenges/tetris.js
+++ frontend/pages/challenges/tetris.js
@@ -73,9 +73,9 @@ const SHAPES = [O_SHAPE, I_SHAPE, S_SHAPE, Z_SHAPE, L_SHAPE, J_SHAPE, T_SHAPE]
 export default function Tetris() {
 	const [game, setGame] = useState({
 		status: 'stopped',
-		board: DEFAULT_BOARD,
-		currentPiece: null,
 		tick: 0,
+		currentPiece: null,
+		board: DEFAULT_BOARD,
 	})
 
 	useEffect(() => {
@@ -83,12 +83,13 @@ export default function Tetris() {
 			return
 		}
 
-		const interval = setInterval(() => updateGame(), 1500)
+		const interval = setInterval(() => updateGame(), 500)
 
 		return () => clearInterval(interval)
-	}, [updateGame])
+	}, [updateGame, game])
 
 	function updateGame() {
+		console.log('game update')
 		if (!game.currentPiece) {
 			spawnPiece()
 		} else {
@@ -101,14 +102,14 @@ export default function Tetris() {
 
 		const newBoard = game.board
 		const newPiece = {
-			x: 0,
+			x: 4,
 			y: 0,
 			shape: SHAPES[Math.floor(Math.random() * SHAPES.length)],
 		}
 
 		for (let y = newPiece.y; y < newPiece.shape.length; y++) {
-			for (let x = newPiece.x; x < newPiece.shape[0].length; x++) {
-				newBoard[y][x] = newPiece.shape[y][x]
+			for (let x = newPiece.x; x < newPiece.shape[0].length + newPiece.x; x++) {
+				newBoard[y][x] = newPiece.shape[y][x - newPiece.x]
 			}
 		}
 
@@ -116,16 +117,46 @@ export default function Tetris() {
 	}
 
 	function shiftCurrentPieceDown() {
-		console.log('shifting piece down')
+		if (!game.currentPiece) {
+			return
+		}
+
+		console.log('shifting piece ↓')
 
 		const newBoard = game.board
 		const newPiece = { ...game.currentPiece, y: game.currentPiece.y + 1 }
 
 		/* check if next y pos is out of bounds or occupied */
 		if (
-			newPiece.y + newPiece.shape.length > newBoard.length ||
-			newBoard[newPiece.y + newPiece.shape.length - 1]?.[0]
+			newPiece.y + newPiece.shape.length >
+			newBoard.length
+			// ||newBoard[newPiece.y + newPiece.shape.length - 1]?.[0]
 		) {
+			newPiece.y = newPiece.y - 1
+
+			for (let y = newPiece.y; y < newPiece.y + newPiece.shape.length; y++) {
+				for (let x = newPiece.x; x < newPiece.x + newPiece.shape[0].length; x++) {
+					if (newPiece.shape[y - newPiece.y][x - newPiece.x] === 1) {
+						newBoard[y][x] = 2
+					}
+				}
+			}
+
+			for (let y = 0; y < newBoard.length; y++) {
+				let completedCells = 0
+
+				for (let x = 0; x < newBoard[y].length; x++) {
+					if (newBoard[y][x]) {
+						completedCells++
+					}
+				}
+
+				if (completedCells === 10) {
+					newBoard.splice(y, 1)
+					newBoard.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+				}
+			}
+
 			setGame((prev) => ({
 				...prev,
 				board: newBoard,
@@ -135,14 +166,101 @@ export default function Tetris() {
 		} else {
 			/* release upper blocks */
 			for (let y = newPiece.y - 1; y < newPiece.y; y++) {
-				for (let x = game.currentPiece.x; x < newPiece.shape[0].length; x++) {
-					newBoard[y][x] = 0
+				for (let x = newPiece.x; x < newPiece.shape[0].length + newPiece.x; x++) {
+					if (newBoard[y][x] !== 2) {
+						newBoard[y][x] = 0
+					}
 				}
 			}
 
 			for (let y = newPiece.y; y < newPiece.y + newPiece.shape.length; y++) {
 				for (let x = newPiece.x; x < newPiece.x + newPiece.shape[0].length; x++) {
-					newBoard[y][x] = newPiece.shape[y - newPiece.y][x - newPiece.x]
+					if (newBoard[y][x] !== 2) {
+						newBoard[y][x] = newPiece.shape[y - newPiece.y][x - newPiece.x]
+					}
+				}
+			}
+
+			setGame((prev) => ({
+				...prev,
+				board: newBoard,
+				tick: prev.tick + 1,
+				currentPiece: newPiece,
+			}))
+		}
+	}
+
+	function shiftCurrentPieceRight() {
+		if (!game.currentPiece) {
+			return
+		}
+
+		console.log('shifting piece ->')
+
+		const newBoard = game.board
+		const newPiece = { ...game.currentPiece, x: game.currentPiece.x + 1 }
+
+		/* check if next y pos is out of bounds or occupied */
+		if (
+			newPiece.x + newPiece.shape[0].length >
+			newBoard[0].length
+			//  ||newBoard[newPiece.y + newPiece.shape.length - 1]?.[0]
+		) {
+			return
+		} else {
+			/* release left side blocks */
+			for (let y = newPiece.y; y < newPiece.y + newPiece.shape.length; y++) {
+				if (newBoard[y][newPiece.x - 1] !== 2) {
+					newBoard[y][newPiece.x - 1] = 0
+				}
+			}
+
+			for (let y = newPiece.y; y < newPiece.y + newPiece.shape.length; y++) {
+				for (let x = newPiece.x; x < newPiece.x + newPiece.shape[0].length; x++) {
+					if (newBoard[y][x] !== 2) {
+						newBoard[y][x] = newPiece.shape[y - newPiece.y][x - newPiece.x]
+					}
+				}
+			}
+
+			setGame((prev) => ({
+				...prev,
+				board: newBoard,
+				tick: prev.tick + 1,
+				currentPiece: newPiece,
+			}))
+		}
+	}
+
+	function shiftCurrentPieceLeft() {
+		if (!game.currentPiece) {
+			return
+		}
+
+		console.log('shifting piece <-')
+
+		const newBoard = game.board
+		const newPiece = { ...game.currentPiece, x: game.currentPiece.x - 1 }
+
+		/* check if next y pos is out of bounds or occupied */
+		if (
+			newPiece.x < 0
+			//  || newBoard[newPiece.y + newPiece.shape.length - 1]?.[0]
+		) {
+			return
+		} else {
+			/* release right side blocks */
+			for (let y = newPiece.y; y < newPiece.y + newPiece.shape.length; y++) {
+				if (newBoard[y][newPiece.x + newPiece.shape[0].length] !== 2) {
+					newBoard[y][newPiece.x + newPiece.shape[0].length] = 0
+				}
+			}
+
+			for (let y = newPiece.y; y < newPiece.y + newPiece.shape.length; y++) {
+				for (let x = newPiece.x; x < newPiece.x + newPiece.shape[0].length; x++) {
+					if (newBoard[y][x] !== 2) {
+						newBoard[y][x] = newPiece.shape[y - newPiece.y][x - newPiece.x]
+					}
 				}
 			}
 
@@ -164,7 +282,15 @@ export default function Tetris() {
 				alignContent: 'center',
 				justifyContent: 'center',
 				gridTemplateColumns: '1fr 1fr',
-				padding: '10px',
+			}}
+			onKeyDown={(e) => {
+				if (e.key === 'ArrowDown') {
+					shiftCurrentPieceDown()
+				} else if (e.key === 'ArrowRight') {
+					shiftCurrentPieceRight()
+				} else if (e.key === 'ArrowLeft') {
+					shiftCurrentPieceLeft()
+				}
 			}}
 		>
 			<div
@@ -193,27 +319,15 @@ export default function Tetris() {
 									textAlign: 'center',
 								}}
 							>
-								{cell}
+								{/* {cell} */}
 							</div>
 						))}
 					</div>
 				))}
 			</div>
 			<div>
-				<div>{JSON.stringify(game)}</div>
 				<button onClick={() => setGame((prev) => ({ ...prev, status: 'running' }))}>START</button>
-				<button
-					onClick={() =>
-						setGame(() => ({
-							status: 'stopped',
-							board: DEFAULT_BOARD,
-							currentPiece: null,
-							tick: 0,
-						}))
-					}
-				>
-					RESET
-				</button>
+				<div>{JSON.stringify(game)}</div>
 			</div>
 		</div>
 	)
